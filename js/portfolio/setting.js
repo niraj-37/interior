7da656ec8909e4a92a8fb76f6934be90ca2509d0
niraj-37 <niraj3gupta@gmail.com>
2018-03-20 16:32:50 +0530

    Interior design project Complete

---
+++ js/portfolio/setting.js
@@ -0,0 +1,230 @@
+
+//number scrolling
+
+
+(function($){
+    $(window).on("load",function(){
+        $(document).scrollzipInit();
+        $(document).rollerInit();
+    });
+    $(window).on("load scroll resize", function(){
+        $('.numscroller').scrollzip({
+            showFunction    :   function() {
+                                    numberRoller($(this).attr('data-slno'));
+                                },
+            wholeVisible    :     false,
+        });
+    });
+    $.fn.scrollzipInit=function(){
+        $('body').prepend("<div style='position:fixed;top:0px;left:0px;width:0;height:0;' id='scrollzipPoint'></div>" );
+    };
+    $.fn.rollerInit=function(){
+        var i=0;
+        $('.numscroller').each(function() {
+            i++;
+           $(this).attr('data-slno',i); 
+           $(this).addClass("roller-title-number-"+i);
+        });        
+    };
+    $.fn.scrollzip = function(options){
+        var settings = $.extend({
+            showFunction    : null,
+            hideFunction    : null,
+            showShift       : 0,
+            wholeVisible    : false,
+            hideShift       : 0,
+        }, options);
+        return this.each(function(i,obj){
+            $(this).addClass('scrollzip');
+            if ( $.isFunction( settings.showFunction ) ){
+                if(
+                    !$(this).hasClass('isShown')&&
+                    ($(window).outerHeight()+$('#scrollzipPoint').offset().top-settings.showShift)>($(this).offset().top+((settings.wholeVisible)?$(this).outerHeight():0))&&
+                    ($('#scrollzipPoint').offset().top+((settings.wholeVisible)?$(this).outerHeight():0))<($(this).outerHeight()+$(this).offset().top-settings.showShift)
+                ){
+                    $(this).addClass('isShown');
+                    settings.showFunction.call( this );
+                }
+            }
+            if ( $.isFunction( settings.hideFunction ) ){
+                if(
+                    $(this).hasClass('isShown')&&
+                    (($(window).outerHeight()+$('#scrollzipPoint').offset().top-settings.hideShift)<($(this).offset().top+((settings.wholeVisible)?$(this).outerHeight():0))||
+                    ($('#scrollzipPoint').offset().top+((settings.wholeVisible)?$(this).outerHeight():0))>($(this).outerHeight()+$(this).offset().top-settings.hideShift))
+                ){
+                    $(this).removeClass('isShown');
+                    settings.hideFunction.call( this );
+                }
+            }
+            return this;
+        });
+    };
+    function numberRoller(slno){
+            var min=$('.roller-title-number-'+slno).attr('data-min');
+            var max=$('.roller-title-number-'+slno).attr('data-max');
+            var timediff=$('.roller-title-number-'+slno).attr('data-delay');
+            var increment=$('.roller-title-number-'+slno).attr('data-increment');
+            var numdiff=max-min;
+            var timeout=(timediff*1000)/numdiff;
+            //if(numinc<10){
+                //increment=Math.floor((timediff*1000)/10);
+            //}//alert(increment);
+            numberRoll(slno,min,max,increment,timeout);
+            
+    }
+    function numberRoll(slno,min,max,increment,timeout){//alert(slno+"="+min+"="+max+"="+increment+"="+timeout);
+        if(min<=max){
+            $('.roller-title-number-'+slno).html(min);
+            min=parseInt(min)+parseInt(increment);
+            setTimeout(function(){numberRoll(eval(slno),eval(min),eval(max),eval(increment),eval(timeout))},timeout);
+        }else{
+            $('.roller-title-number-'+slno).html(max);
+        }
+    }
+})(jQuery);
+
+
+
+//end of scrolling
+
+
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 100) {
+	    $(".navbar-default").css("background" , "#f3aa3d");
+		$(".navbar-nav>li>a").css("color" , "#fff");
+		$(".navbar-nav>li>active").css("color" , "#f3aa3d");
+	  }
+
+	  else{
+		  $(".navbar-default").css("background" , "#fff"); 
+		  $(".navbar-nav>li>a").css("color" , "black");
+		  $(".navbar-nav>li>active").css("color" , "#f3aa3d");
+	  }
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+ $(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+ 
+ 
+ 
+ 
+ 
+ 
+ jQuery(document).ready(function($) {
+ 
+        $('#myCarousel').carousel({
+                interval: 5000
+        });
+ 
+        $('#carousel-text').html($('#slide-content-0').html());
+ 
+        //Handles the carousel thumbnails
+       $('[id^=carousel-selector-]').click( function(){
+            var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+            var id = parseInt(id);
+            $('#myCarousel').carousel(id);
+        });
+ 
+ 
+        // When the carousel slides, auto update the text
+        $('#myCarousel').on('slid.bs.carousel', function (e) {
+                 var id = $('.item.active').data('slide-number');
+                $('#carousel-text').html($('#slide-content-'+id).html());
+        });
+});
+
+
+
+
+
+
+$(document).ready(function() {
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pause: true,
+    interval: 4000,
+  });
+});
+
+jQuery(document).ready(function($){
+
+if (jQuery().quicksand) {
+
+ 	// Clone applications to get a second collection
+	var $data = $(".portfolio").clone();
+	
+	//NOTE: Only filter on the main portfolio page, not on the subcategory pages
+	$('.filter li').click(function(e) {
+		$(".filter li").removeClass("active");	
+		// Use the last category class as the category to filter by. This means that multiple categories are not supported (yet)
+		var filterClass=$(this).attr('class').split(' ').slice(-1)[0];
+		
+		if (filterClass == 'all') {
+			var $filteredData = $data.find('.item-thumbs');
+		} else {
+			var $filteredData = $data.find('.item-thumbs[data-type=' + filterClass + ']');
+		}
+		$(".portfolio").quicksand($filteredData, {
+			duration: 600,
+			adjustHeight: 'auto'
+		}, function () {
+		
+			// Portfolio fancybox
+		$(".fancybox").fancybox({				
+				padding : 0,
+				beforeShow: function () {
+					this.title = $(this.element).attr('title');
+					this.title = '<h4>' + this.title + '</h4>' + '<p>' + $(this.element).parent().find('img').attr('alt') + '</p>';
+				},
+				helpers : {
+					title : { type: 'inside' },
+				}
+			});
+
+
+		});	
+		$(this).addClass("active"); 			
+		return false;
+	});
+	
+}//if quicksand
+
+});
